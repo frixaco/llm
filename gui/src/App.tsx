61a3b83c7a4d5823050e4f1ef05ff6baaf9f6a5b
frixaco <rustam21ashurmatov@gmail.com>
2025-04-11 10:30:28 +0500

    try reading file system in gui

--- gui/src/App.tsx
+++ gui/src/App.tsx
@@ -1,49 +1,56 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
+import { useEffect, useState } from "react";
 import "./App.css";
 
-function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+import { readDir, BaseDirectory, DirEntry } from "@tauri-apps/plugin-fs";
+import { join } from "@tauri-apps/api/path";
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
+function App() {
+  const [failure, setError] = useState("");
+  const [dirs, setDirs] = useState<string[]>([]);
+
+  useEffect(() => {
+    async function processEntriesRecursively(
+      parent: string,
+      entries: DirEntry[],
+    ) {
+      for (const entry of entries) {
+        if (entry.isDirectory) {
+          setDirs((p) => [...p, entry.name]);
+          const dir = await join(parent, entry.name);
+          processEntriesRecursively(
+            dir,
+            await readDir(dir, { baseDir: BaseDirectory.Home }),
+          );
+        }
+      }
+    }
+
+    const check = async () => {
+      try {
+        const dir = "personal";
+        const entries = await readDir(dir, {
+          baseDir: BaseDirectory.Home,
+        });
+
+        processEntriesRecursively(dir, entries);
+      } catch (err) {
+        setError(String(err));
+      }
+    };
+    check();
+  }, []);
 
   return (
     <main className="container">
-      <h1>Welcome to Tauri + React</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="dirs">
+        {dirs.map((d, i) => (
+          <div className="dir">
+            <p key={`${d}_${i}`}>{d}</p>
+          </div>
+        ))}
       </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
 
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
+      <p>{failure}</p>
     </main>
   );
 }
