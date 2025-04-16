b8310700ffbf8d29a14066e626cd7365d668c802
frixaco <rustam21ashurmatov@gmail.com>
2025-04-17 01:51:23 +0500

    get project root dir and show a file from there

--- gui/src-tauri/src/lib.rs
+++ gui/src-tauri/src/lib.rs
@@ -1,7 +1,36 @@
+use std::env;
+
+use git2::Repository;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn get_project_dir(path: Option<String>) -> String {
+    let target_path = match path {
+        Some(p) => p,
+        None => env::current_dir()
+            .map(|path| path.to_string_lossy().into_owned())
+            .unwrap_or_else(|_| "Unknown".to_string()),
+    };
+    println!("Target path: {}", target_path.to_string());
+    match Repository::discover(&target_path) {
+        Ok(repo) => match repo.workdir() {
+            Some(workdir) => match workdir.to_str() {
+                Some(wd_path) => {
+                    println!("Git dir: {}", wd_path);
+                    wd_path.to_string()
+                }
+                None => {
+                    eprintln!("Failed to convert workdir to string");
+                    "Unknown".to_string()
+                }
+            },
+            None => {
+                eprintln!("Failed to convert repo path to string");
+                "Unknown".to_string()
+            }
+        },
+        Err(_) => "Unknown".to_string(),
+    }
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -10,7 +39,7 @@ pub fn run() {
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![get_project_dir])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- gui/src/App.tsx
+++ gui/src/App.tsx
@@ -1,44 +1,50 @@
 import { createEffect, createSignal } from "solid-js";
 import "./App.css";
 
-import { readTextFile } from "@tauri-apps/plugin-fs";
+import { readTextFile, readDir, BaseDirectory } from "@tauri-apps/plugin-fs";
 import { CodeBlock } from "./components/codeblock";
-import { exit } from '@tauri-apps/plugin-process';
+import { exit } from "@tauri-apps/plugin-process";
+import { invoke } from "@tauri-apps/api/core";
 
-const file = "D:/dev/llm/cli-go/cli.go"
-
+// const file = "D:/dev/llm/cli-go/cli.go";
 
 function App() {
+  const [projectDir, setProjectDir] = createSignal<string | null>(null);
   const [code, setCode] = createSignal("");
-  const [prompt, setPrompt] = createSignal("")
+  const [prompt, setPrompt] = createSignal("");
 
   const onClick = async () => {
-    console.log("onClick")
-    const content = await readTextFile(file)
-    setCode(content)
-  }
+    const cwd: string = await invoke("get_project_dir", {});
+    console.log("projectDir", cwd);
+    setProjectDir(cwd);
+    const content = await readTextFile(cwd + "/cli-go/main.go");
+    setCode(content);
+  };
 
   createEffect(() => {
     const evl = async (e: KeyboardEvent) => {
-      if (e.key === "Q" && e.shiftKey === true && e.target!.tagName === "BODY") {
-        await exit(1)
+      if (
+        e.key === "Q" &&
+        e.shiftKey === true &&
+        e.target!.tagName === "BODY"
+      ) {
+        await exit(1);
       }
-    }
+    };
 
-    document.addEventListener("keypress", evl)
+    document.addEventListener("keypress", evl);
 
     return () => {
-      document.removeEventListener("keypress", evl)
-    }
-  })
+      document.removeEventListener("keypress", evl);
+    };
+  });
 
   return (
     <main class="container">
-      <button onClick={onClick}>READ FILE</button>
+      <button onClick={onClick}>Get cwd and read a file</button>
       <CodeBlock code={code()} />
 
       <input onInput={(e) => setPrompt(e.currentTarget.value)} />
-
 
       <p>{prompt()}</p>
     </main>
